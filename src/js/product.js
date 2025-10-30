a482acdc6770db60cc7d50449ccd57e4a2dff051
KristinLind <kristin.lind@comcast.net>
2025-10-30 09:09:40 -0600

    Fix: Add to Cart now stores multiple items in so-cart array

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,17 @@
-import { setLocalStorage } from './utils.mjs';
-import ProductData from './ProductData.mjs';
+import ProductData from "./ProductData.mjs";
+import { getLocalStorage, setLocalStorage, qs } from "./utils.mjs";
 
-const dataSource = new ProductData('tents');
+const dataSource = new ProductData("tents");
 
+//add products without overwriting previous cart
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+  const KEY = "so-cart";
+  const current = getLocalStorage(KEY);
+  const cart = Array.isArray(current) ? current : current ? [current] : [];
+  cart.push(product);
+  setLocalStorage(KEY, cart);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
@@ -14,5 +20,5 @@ async function addToCartHandler(e) {
 
 // add listener to Add to Cart button
 document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
